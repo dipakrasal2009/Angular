9a6beeb6d4ee6128255eacc5f0d8ad27c6ce3462
Dipak Rasal <133906634+dipakrasal2009@users.noreply.github.com>
2024-01-06 20:00:44 +0530

    Create Component and Display the Output

---
+++ src/app/marvellous/marvellous.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marvellous',
+  standalone: true,
+  imports: [],
+  templateUrl: './marvellous.component.html',
+  styleUrl: './marvellous.component.css'
+})
+export class MarvellousComponent {
+
+}
